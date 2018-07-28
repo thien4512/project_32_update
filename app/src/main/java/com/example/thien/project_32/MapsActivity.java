77ca5fc627e6bb7115f4f8a45a88f3af6800fac2
Thien4512 <thien4512@gmail.com>
2018-07-28 23:37:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/thien/project_32/MapsActivity.java
@@ -0,0 +1,103 @@
+package com.example.thien.project_32;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    ArrayList<LatLng> mylist_ll = new ArrayList<LatLng>();
+    private LatLng tt;
+    LatLng my_ll = new LatLng(10.774136, 106.651757);
+    LatLng my_ll_2 = new LatLng(10.776136, 106.654757);
+    Button Clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        Clear = (Button) findViewById(R.id.clear_position);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMap.clear();
+            }
+        });
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(10.773136, 106.651757);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("My address"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.addMarker(new MarkerOptions().position(my_ll).title("My Address 2"));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+        mylist_ll.add(sydney);
+        mylist_ll.add(my_ll);
+        mylist_ll.add(my_ll_2);
+        drawPolyLineOnMap(mylist_ll);
+
+    }
+
+    // Draw polyline on map
+    public void drawPolyLineOnMap(List<LatLng>list) {
+        PolylineOptions polyOptions = new PolylineOptions();
+        polyOptions.color(Color.RED);
+        polyOptions.width(5);
+        polyOptions.addAll(list);
+
+        mMap.clear();
+        mMap.addPolyline(polyOptions);
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (LatLng latLng : list) {
+            builder.include(latLng);
+        }
+
+        final LatLngBounds bounds = builder.build();
+        mMap.addMarker(new MarkerOptions().position(list.get(0)).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_action_name_2)).title("My address"));
+        mMap.addMarker(new MarkerOptions().position(list.get(list.size()-1)).title("My address 2"));
+
+        //BOUND_PADDING is an int to specify padding of bound.. try 100.
+        //CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds,30);
+       // mMap.animateCamera(cu);
+    }
+
+}
